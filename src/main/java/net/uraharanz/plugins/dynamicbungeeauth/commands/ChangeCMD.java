9520ebfe289f32ef52869b522a0d290583b94eeb
MindsMaster <1173348610@qq.com>
2025-11-11 15:13:36 +0800

    refactor: refactor ChangeCMD.java

--- src/main/java/net/uraharanz/plugins/dynamicbungeeauth/commands/ChangeCMD.java
+++ src/main/java/net/uraharanz/plugins/dynamicbungeeauth/commands/ChangeCMD.java
@@ -9,74 +9,145 @@ import net.uraharanz.plugins.dynamicbungeeauth.utils.messages.MessageHandler;
 import net.uraharanz.plugins.dynamicbungeeauth.utils.mysql.SQL;
 import net.uraharanz.plugins.dynamicbungeeauth.utils.password.HashMethods;
 
-public class ChangeCMD
-extends Command {
-    private main plugin;
-    private int minPass;
-    private int maxPass;
+/**
+ * @author an5w1r@163.com
+ */
+public class ChangeCMD extends Command {
+    private final main plugin;
+    private final int minPasswordLength;
+    private final int maxPasswordLength;
 
-    public ChangeCMD(main main2) {
+    public ChangeCMD(main plugin) {
         super("changepassword", "auth.changepassword", "cpw");
-        this.plugin = main2;
-        this.maxPass = main2.getConfigLoader().getIntegerCFG("Options.MaxPasswordLength");
-        this.minPass = main2.getConfigLoader().getIntegerCFG("Options.MinPasswordLength");
+        this.plugin = plugin;
+        this.maxPasswordLength = plugin.getConfigLoader().getIntegerCFG("Options.MaxPasswordLength");
+        this.minPasswordLength = plugin.getConfigLoader().getIntegerCFG("Options.MinPasswordLength");
     }
 
-    public void execute(CommandSender commandSender, final String[] stringArray) {
-        this.plugin.getProxy().getScheduler().runAsync(this.plugin, () -> {
-            if (commandSender instanceof ProxiedPlayer) {
-                final ProxiedPlayer proxiedPlayer = (ProxiedPlayer)commandSender;
-                if (stringArray.length < 2) {
-                    proxiedPlayer.sendMessage(MessageHandler.sendMSG(this.plugin.getConfigLoader().getStringMSG("Commands.changepassword.wrong")));
+    @Override
+    public void execute(CommandSender sender, final String[] args) {
+        plugin.getProxy().getScheduler().runAsync(plugin, () -> {
+            if (!(sender instanceof ProxiedPlayer)) {
+                return;
+            }
+
+            final ProxiedPlayer player = (ProxiedPlayer) sender;
+            processPasswordChange(player, args);
+        });
+    }
+
+    private void processPasswordChange(final ProxiedPlayer player, final String[] args) {
+        if (!validateCommandFormat(player, args)) {
+            return;
+        }
+
+        checkPlayerRegistration(player, args);
+    }
+
+    private boolean validateCommandFormat(ProxiedPlayer player, String[] args) {
+        if (args.length < 2) {
+            sendMessage(player, "Commands.changepassword.wrong");
+            return false;
+        }
+        return true;
+    }
+
+    private void checkPlayerRegistration(final ProxiedPlayer player, final String[] args) {
+        SQL.isPlayerDB(player, new CallbackSQL<Boolean>() {
+            @Override
+            public void done(Boolean isRegistered) {
+                if (!isRegistered) {
+                    sendMessage(player, "Commands.changepassword.not");
                     return;
                 }
-                SQL.isPlayerDB(proxiedPlayer, new CallbackSQL<Boolean>(){
 
-                    @Override
-                    public void done(Boolean bl) {
-                        if (!bl) {
-                            proxiedPlayer.sendMessage(MessageHandler.sendMSG(ChangeCMD.this.plugin.getConfigLoader().getStringMSG("Commands.changepassword.not")));
-                            return;
-                        }
-                        if (stringArray[0].equals(stringArray[1])) {
-                            proxiedPlayer.sendMessage(MessageHandler.sendMSG(ChangeCMD.this.plugin.getConfigLoader().getStringMSG("Commands.changepassword.same")));
-                            return;
-                        }
-                        SQL.getPlayerDataS(proxiedPlayer, "password", new CallbackSQL<String>(){
+                validatePasswordChange(player, args);
+            }
 
-                            @Override
-                            public void done(final String string) {
-                                SQL.getPlayerDataS(proxiedPlayer, "salt", new CallbackSQL<String>(){
-
-                                    @Override
-                                    public void done(String string4) {
-                                        String string2 = HashMethods.HashPassword(proxiedPlayer, stringArray[0], string4);
-                                        if (string.equals(string2)) {
-                                            String string3 = HashMethods.HashPassword(proxiedPlayer, stringArray[1], string4);
-                                            SQL.setPlayerData(proxiedPlayer, "password", string3);
-                                            proxiedPlayer.sendMessage(MessageHandler.sendMSG(ChangeCMD.this.plugin.getConfigLoader().getStringMSG("Commands.changepassword.successful")));
-                                            return;
-                                        }
-                                        proxiedPlayer.sendMessage(MessageHandler.sendMSG(ChangeCMD.this.plugin.getConfigLoader().getStringMSG("Commands.changepassword.incorrect")));
-                                    }
-
-                                    @Override
-                                    public void error(Exception exception) {
-                                    }
-                                });
-                            }
-
-                            @Override
-                            public void error(Exception exception) {
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void error(Exception exception) {
-                    }
-                });
+            @Override
+            public void error(Exception e) {
+                e.printStackTrace();
             }
         });
+    }
+
+    private void validatePasswordChange(final ProxiedPlayer player, final String[] args) {
+        String oldPassword = args[0];
+        String newPassword = args[1];
+
+        if (oldPassword.equals(newPassword)) {
+            sendMessage(player, "Commands.changepassword.same");
+            return;
+        }
+
+        if (!validateNewPasswordLength(player, newPassword)) {
+            return;
+        }
+
+        verifyCurrentPassword(player, oldPassword, newPassword);
+    }
+
+    private boolean validateNewPasswordLength(ProxiedPlayer player, String newPassword) {
+        if (newPassword.length() < minPasswordLength) {
+            sendMessage(player, "Commands.changepassword.too_short");
+            return false;
+        }
+
+        if (newPassword.length() > maxPasswordLength) {
+            sendMessage(player, "Commands.changepassword.too_long");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void verifyCurrentPassword(final ProxiedPlayer player,
+                                       final String oldPassword,
+                                       final String newPassword) {
+        SQL.getPlayerDataS(player, "password", new CallbackSQL<String>() {
+            @Override
+            public void done(final String storedPassword) {
+                getSaltAndVerify(player, oldPassword, newPassword, storedPassword);
+            }
+
+            @Override
+            public void error(Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void getSaltAndVerify(final ProxiedPlayer player,
+                                  final String oldPassword,
+                                  final String newPassword,
+                                  final String storedPassword) {
+        SQL.getPlayerDataS(player, "salt", new CallbackSQL<String>() {
+            @Override
+            public void done(String salt) {
+                String hashedOldPassword = HashMethods.HashPassword(player, oldPassword, salt);
+
+                if (storedPassword.equals(hashedOldPassword)) {
+                    updatePassword(player, newPassword, salt);
+                } else {
+                    sendMessage(player, "Commands.changepassword.incorrect");
+                }
+            }
+
+            @Override
+            public void error(Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void updatePassword(ProxiedPlayer player, String newPassword, String salt) {
+        String hashedNewPassword = HashMethods.HashPassword(player, newPassword, salt);
+        SQL.setPlayerData(player, "password", hashedNewPassword);
+        sendMessage(player, "Commands.changepassword.successful");
+    }
+
+    private void sendMessage(ProxiedPlayer player, String messageKey) {
+        String message = plugin.getConfigLoader().getStringMSG(messageKey);
+        player.sendMessage(MessageHandler.sendMSG(message));
     }
 }
