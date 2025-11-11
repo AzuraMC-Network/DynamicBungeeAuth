eefb66bcbb0c8cd9a0d47946f483ebbf883541a9
MindsMaster <1173348610@qq.com>
2025-11-11 15:48:23 +0800

    refactor: refactor PremiumCMD.java

--- src/main/java/net/uraharanz/plugins/dynamicbungeeauth/commands/PremiumCMD.java
+++ src/main/java/net/uraharanz/plugins/dynamicbungeeauth/commands/PremiumCMD.java
@@ -1,10 +1,8 @@
 package net.uraharanz.plugins.dynamicbungeeauth.commands;
 
-import java.util.UUID;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
-import net.md_5.bungee.api.plugin.Plugin;
 import net.uraharanz.plugins.dynamicbungeeauth.cache.cache.PlayerCache;
 import net.uraharanz.plugins.dynamicbungeeauth.main;
 import net.uraharanz.plugins.dynamicbungeeauth.utils.callback.CallbackAPI;
@@ -12,175 +10,211 @@ import net.uraharanz.plugins.dynamicbungeeauth.utils.callback.CallbackSQL;
 import net.uraharanz.plugins.dynamicbungeeauth.utils.messages.MessageHandler;
 import net.uraharanz.plugins.dynamicbungeeauth.utils.mysql.SQL;
 
-public class PremiumCMD
-extends Command {
-    private main plugin;
-    private int Method;
-    private int MethodType;
+import java.util.UUID;
 
-    public PremiumCMD(main main2) {
+/**
+ * @author an5w1r@163.com
+ */
+public class PremiumCMD extends Command {
+    private final main plugin;
+    private final int workMethod;
+    private final int premiumMode;
+
+    public PremiumCMD(main plugin) {
         super("premium", "auth.premium");
-        this.plugin = main2;
-        this.Method = main2.getConfigLoader().getIntegerCFG("WorkMethod.Value");
-        this.MethodType = main2.getConfigLoader().getIntegerCFG("Commands.PremiumMode");
+        this.plugin = plugin;
+        this.workMethod = plugin.getConfigLoader().getIntegerCFG("WorkMethod.Value");
+        this.premiumMode = plugin.getConfigLoader().getIntegerCFG("Commands.PremiumMode");
     }
 
-    public void execute(final CommandSender commandSender, String[] stringArray) {
-        if (this.plugin.getConfigLoader().getBooleanCFG("Commands.PremiumCMD")) {
-            if (this.Method == 3) {
-                commandSender.sendMessage(MessageHandler.sendMSG(this.plugin.getConfigLoader().getStringMSG("Commands.premium.wrong_method")));
-                return;
-            }
-        } else {
-            commandSender.sendMessage(MessageHandler.sendMSG(this.plugin.getConfigLoader().getStringMSG("Commands.premium.deactivated")));
+    @Override
+    public void execute(final CommandSender sender, String[] args) {
+        if (!isCommandAvailable(sender)) {
             return;
         }
-        this.plugin.getProxy().getScheduler().runAsync(this.plugin, () -> {
-            if (!(commandSender instanceof ProxiedPlayer)) {
-                commandSender.sendMessage(MessageHandler.sendMSG(this.plugin.getConfigLoader().getStringMSG("Commands.premium.only_player")));
-                return;
-            }
-            final ProxiedPlayer proxiedPlayer = (ProxiedPlayer)commandSender;
-            this.plugin.getProfileGenerator().Generator(commandSender.getName(), new CallbackAPI<UUID>(){
 
-                @Override
-                public void done(UUID uUID) {
-                    if (uUID == null) {
-                        proxiedPlayer.sendMessage(MessageHandler.sendMSG(PremiumCMD.this.plugin.getConfigLoader().getStringMSG("Commands.premium.not_mojang")));
-                        return;
-                    }
-                    SQL.isPlayerDB(proxiedPlayer, new CallbackSQL<Boolean>(){
-
-                        @Override
-                        public void done(Boolean bl) {
-                            if (bl) {
-                                if (PremiumCMD.this.MethodType != 1) {
-                                    SQL.getPlayerDataS(proxiedPlayer, "valid", new CallbackSQL<String>(){
-
-                                        @Override
-                                        public void done(String string) {
-                                            if (string.equals("1")) {
-                                                SQL.getPlayerDataS(proxiedPlayer, "premium", new CallbackSQL<String>(){
-
-                                                    @Override
-                                                    public void done(String string) {
-                                                        if (string.equalsIgnoreCase("1")) {
-                                                            proxiedPlayer.sendMessage(MessageHandler.sendMSG(PremiumCMD.this.plugin.getConfigLoader().getStringMSG("Commands.premium.already_premium")));
-                                                            return;
-                                                        }
-                                                        SQL.RemovePlayerDB(proxiedPlayer, new CallbackSQL<Boolean>(){
-
-                                                            @Override
-                                                            public void done(Boolean bl) {
-                                                                if (!bl) {
-                                                                    commandSender.sendMessage(MessageHandler.sendMSG(PremiumCMD.this.plugin.getConfigLoader().getStringMSG("Commands.premium.error")));
-                                                                    return;
-                                                                }
-                                                                SQL.isName(proxiedPlayer.getName(), new CallbackSQL<Boolean>(){
-
-                                                                    @Override
-                                                                    public void done(Boolean bl) {
-                                                                        if (bl) {
-                                                                            commandSender.sendMessage(MessageHandler.sendMSG(PremiumCMD.this.plugin.getConfigLoader().getStringMSG("Commands.premium.on_list")));
-                                                                            return;
-                                                                        }
-                                                                        SQL.addName(proxiedPlayer.getName());
-                                                                        PremiumCMD.this.plugin.getPlayerDataList().removePlayer(proxiedPlayer.getName());
-                                                                        PremiumCMD.this.plugin.getPlayerCacheList().removeCache(proxiedPlayer.getName());
-                                                                        commandSender.sendMessage(MessageHandler.sendMSG(PremiumCMD.this.plugin.getConfigLoader().getStringMSG("Commands.premium.successful")));
-                                                                        proxiedPlayer.disconnect(MessageHandler.sendMSG(PremiumCMD.this.plugin.getConfigLoader().getStringMSG("KickMessages.PremiumKick")));
-                                                                    }
-
-                                                                    @Override
-                                                                    public void error(Exception exception) {
-                                                                    }
-                                                                });
-                                                            }
-
-                                                            @Override
-                                                            public void error(Exception exception) {
-                                                            }
-                                                        });
-                                                    }
-
-                                                    @Override
-                                                    public void error(Exception exception) {
-                                                    }
-                                                });
-                                            } else {
-                                                proxiedPlayer.sendMessage(MessageHandler.sendMSG(PremiumCMD.this.plugin.getConfigLoader().getStringMSG("Commands.premium.require_login")));
-                                            }
-                                        }
-
-                                        @Override
-                                        public void error(Exception exception) {
-                                        }
-                                    });
-                                } else {
-                                    SQL.getPlayerDataS(proxiedPlayer, "premium", new CallbackSQL<String>(){
-
-                                        @Override
-                                        public void done(String string) {
-                                            if (string.equalsIgnoreCase("1")) {
-                                                proxiedPlayer.sendMessage(MessageHandler.sendMSG(PremiumCMD.this.plugin.getConfigLoader().getStringMSG("Commands.premium.already_premium")));
-                                                return;
-                                            }
-                                            SQL.RemovePlayerDB(proxiedPlayer, new CallbackSQL<Boolean>(){
-
-                                                @Override
-                                                public void done(Boolean bl) {
-                                                    if (!bl) {
-                                                        commandSender.sendMessage(MessageHandler.sendMSG(PremiumCMD.this.plugin.getConfigLoader().getStringMSG("Commands.premium.error")));
-                                                        return;
-                                                    }
-                                                    SQL.isName(proxiedPlayer.getName(), new CallbackSQL<Boolean>(){
-
-                                                        @Override
-                                                        public void done(Boolean bl) {
-                                                            if (bl) {
-                                                                commandSender.sendMessage(MessageHandler.sendMSG(PremiumCMD.this.plugin.getConfigLoader().getStringMSG("Commands.premium.on_list")));
-                                                                return;
-                                                            }
-                                                            SQL.addName(proxiedPlayer.getName());
-                                                            PremiumCMD.this.plugin.getPlayerDataList().removePlayer(proxiedPlayer.getName());
-                                                            PremiumCMD.this.plugin.getPlayerCacheList().removeCache(proxiedPlayer.getName());
-                                                            commandSender.sendMessage(MessageHandler.sendMSG(PremiumCMD.this.plugin.getConfigLoader().getStringMSG("Commands.premium.successful")));
-                                                            proxiedPlayer.disconnect(MessageHandler.sendMSG(PremiumCMD.this.plugin.getConfigLoader().getStringMSG("KickMessages.PremiumKick")));
-                                                        }
-
-                                                        @Override
-                                                        public void error(Exception exception) {
-                                                        }
-                                                    });
-                                                }
-
-                                                @Override
-                                                public void error(Exception exception) {
-                                                }
-                                            });
-                                        }
-
-                                        @Override
-                                        public void error(Exception exception) {
-                                        }
-                                    });
-                                }
-                            } else {
-                                PlayerCache playerCache = main.plugin.getPlayerCacheList().searchCache(proxiedPlayer.getName());
-                                commandSender.sendMessage(MessageHandler.sendMSG(PremiumCMD.this.plugin.getConfigLoader().getStringMSG("Commands.premium.not_registered").replaceAll("%captcha%", playerCache.getCaptcha())));
-                            }
-                        }
-
-                        @Override
-                        public void error(Exception exception) {
-                        }
-                    });
-                }
-
-                @Override
-                public void error(Exception exception) {
-                }
-            });
+        plugin.getProxy().getScheduler().runAsync(plugin, () -> {
+            processPremiumConversion(sender);
         });
+    }
+
+    private boolean isCommandAvailable(CommandSender sender) {
+        if (!plugin.getConfigLoader().getBooleanCFG("Commands.PremiumCMD")) {
+            sendMessage(sender, "Commands.premium.deactivated");
+            return false;
+        }
+
+        if (workMethod == 3) {
+            sendMessage(sender, "Commands.premium.wrong_method");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void processPremiumConversion(final CommandSender sender) {
+        if (!validatePlayerSender(sender)) {
+            return;
+        }
+
+        final ProxiedPlayer player = (ProxiedPlayer) sender;
+
+        verifyMojangAccount(player);
+    }
+
+    private boolean validatePlayerSender(CommandSender sender) {
+        if (!(sender instanceof ProxiedPlayer)) {
+            sendMessage(sender, "Commands.premium.only_player");
+            return false;
+        }
+        return true;
+    }
+
+    private void verifyMojangAccount(final ProxiedPlayer player) {
+        plugin.getProfileGenerator().Generator(player.getName(), new CallbackAPI<UUID>() {
+            @Override
+            public void done(UUID uuid) {
+                if (uuid == null) {
+                    sendMessage(player, "Commands.premium.not_mojang");
+                    return;
+                }
+
+                checkPlayerRegistration(player);
+            }
+
+            @Override
+            public void error(Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void checkPlayerRegistration(final ProxiedPlayer player) {
+        SQL.isPlayerDB(player, new CallbackSQL<Boolean>() {
+            @Override
+            public void done(Boolean isRegistered) {
+                if (!isRegistered) {
+                    handleUnregisteredPlayer(player);
+                    return;
+                }
+
+                processPremiumModeConversion(player);
+            }
+
+            @Override
+            public void error(Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void handleUnregisteredPlayer(ProxiedPlayer player) {
+        PlayerCache cache = plugin.getPlayerCacheList().searchCache(player.getName());
+        String message = plugin.getConfigLoader()
+                .getStringMSG("Commands.premium.not_registered")
+                .replaceAll("%captcha%", cache.getCaptcha());
+        player.sendMessage(MessageHandler.sendMSG(message));
+    }
+
+    private void processPremiumModeConversion(final ProxiedPlayer player) {
+        if (premiumMode != 1) {
+            checkLoginRequirement(player);
+        } else {
+            checkPremiumStatus(player);
+        }
+    }
+
+    private void checkLoginRequirement(final ProxiedPlayer player) {
+        SQL.getPlayerDataS(player, "valid", new CallbackSQL<String>() {
+            @Override
+            public void done(String validStatus) {
+                if (!validStatus.equals("1")) {
+                    sendMessage(player, "Commands.premium.require_login");
+                    return;
+                }
+
+                checkPremiumStatus(player);
+            }
+
+            @Override
+            public void error(Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void checkPremiumStatus(final ProxiedPlayer player) {
+        SQL.getPlayerDataS(player, "premium", new CallbackSQL<String>() {
+            @Override
+            public void done(String premiumStatus) {
+                if (premiumStatus.equalsIgnoreCase("1")) {
+                    sendMessage(player, "Commands.premium.already_premium");
+                    return;
+                }
+
+                removePlayerFromDatabase(player);
+            }
+
+            @Override
+            public void error(Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void removePlayerFromDatabase(final ProxiedPlayer player) {
+        SQL.RemovePlayerDB(player, new CallbackSQL<Boolean>() {
+            @Override
+            public void done(Boolean success) {
+                if (!success) {
+                    sendMessage(player, "Commands.premium.error");
+                    return;
+                }
+
+                checkNameList(player);
+            }
+
+            @Override
+            public void error(Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void checkNameList(final ProxiedPlayer player) {
+        SQL.isName(player.getName(), new CallbackSQL<Boolean>() {
+            @Override
+            public void done(Boolean isOnList) {
+                if (isOnList) {
+                    sendMessage(player, "Commands.premium.on_list");
+                    return;
+                }
+
+                completePremiumConversion(player);
+            }
+
+            @Override
+            public void error(Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void completePremiumConversion(ProxiedPlayer player) {
+        // add to premium list
+        SQL.addName(player.getName());
+
+        plugin.getPlayerDataList().removePlayer(player.getName());
+        plugin.getPlayerCacheList().removeCache(player.getName());
+
+        sendMessage(player, "Commands.premium.successful");
+
+        String kickMessage = plugin.getConfigLoader().getStringMSG("KickMessages.PremiumKick");
+        player.disconnect(MessageHandler.sendMSG(kickMessage));
+    }
+
+    private void sendMessage(CommandSender sender, String messageKey) {
+        String message = plugin.getConfigLoader().getStringMSG(messageKey);
+        sender.sendMessage(MessageHandler.sendMSG(message));
     }
 }
