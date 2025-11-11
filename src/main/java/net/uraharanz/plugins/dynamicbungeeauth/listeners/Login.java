2d6762ed96c9c2bb2a6aa042f7a4f14e3770cf15
MindsMaster <1173348610@qq.com>
2025-11-11 20:46:35 +0800

    refactor: refactor Login.java

--- src/main/java/net/uraharanz/plugins/dynamicbungeeauth/listeners/Login.java
+++ src/main/java/net/uraharanz/plugins/dynamicbungeeauth/listeners/Login.java
@@ -3,50 +3,43 @@ package net.uraharanz.plugins.dynamicbungeeauth.listeners;
 import net.md_5.bungee.api.connection.PendingConnection;
 import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
-import net.md_5.bungee.connection.InitialHandler;
 import net.md_5.bungee.event.EventHandler;
 import net.uraharanz.plugins.dynamicbungeeauth.DBAPlugin;
 import net.uraharanz.plugins.dynamicbungeeauth.utils.mysql.SQL;
 import org.geysermc.floodgate.api.FloodgateApi;
 
-import java.lang.reflect.Field;
 import java.util.UUID;
 
-public class Login
-implements Listener {
-    private DBAPlugin plugin;
-    private boolean offlineUUID;
-    private FloodgateApi floodgateApi;
+/**
+ * @author an5w1r@163.com
+ */
+public class Login implements Listener {
+    private final DBAPlugin plugin;
+    private final boolean offlineUUID;
 
     public Login(DBAPlugin plugin) {
         this.plugin = plugin;
         this.offlineUUID = plugin.getConfigLoader().getBooleanCFG("Options.OfflineUUID");
     }
 
+    public static UUID generateOfflineUUID(String username) {
+        return SQL.generateOfflineUUID(username);
+    }
+
     @EventHandler(priority=-64)
-    public void onLogin(LoginEvent loginEvent) {
-        if (loginEvent.isCancelled()) {
+    public void onLogin(LoginEvent event) {
+        if (event.isCancelled()) {
             return;
         }
         if (this.offlineUUID) {
-            if (this.plugin.getFloodgateApi() != null && this.plugin.getFloodgateApi().getPlayer(loginEvent.getConnection().getUniqueId()) != null) {
+            final FloodgateApi floodgate = this.plugin.getFloodgateApi();
+            if (floodgate != null && floodgate.getPlayer(event.getConnection().getUniqueId()) != null) {
                 return;
             }
-            PendingConnection pendingConnection = loginEvent.getConnection();
-            InitialHandler initialHandler = (InitialHandler)pendingConnection;
-            String string = pendingConnection.getName();
-            try {
-                Field field = InitialHandler.class.getDeclaredField("uniqueId");
-                field.setAccessible(true);
-                field.set(pendingConnection, Login.generateOfflineUUID(string));
-            }
-            catch (IllegalAccessException | NoSuchFieldException reflectiveOperationException) {
-                reflectiveOperationException.printStackTrace();
-            }
+            final PendingConnection connection = event.getConnection();
+            final String username = connection.getName();
+            // original code used reflection, but I don't know why, I am not sure use connection.setUniqueId() hasn't issue.
+            connection.setUniqueId(Login.generateOfflineUUID(username));
         }
-    }
-
-    public static UUID generateOfflineUUID(String string) {
-        return SQL.generateOfflineUUID(string);
     }
 }
